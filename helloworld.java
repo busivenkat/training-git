09d3faa43d4ff9312707badcbc5c309cb2a12382
busivenkat <busivenkat@gmail.com>
2018-11-17 14:06:24 +0000

    commit for modified file

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
- class HelloWorld {
+class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
    }
 }
-
